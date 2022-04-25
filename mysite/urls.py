5b08f8af11e6f801b3ae49143714644200502199
mohit <mohit542002@gmail.com>
2022-04-25 14:42:57 +0530

    Heroku deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import url
 from django.urls import path
+from django.views.static import serve
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="Home"),
     path('analyze',views.analyze,name="analyze"),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
